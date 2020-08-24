53d6e264ac734b3d799ad98f54c756713fe33e34
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-24 19:36:39 +0800

    added filter throug url queries: able to sort

--- controllers/bootcamps/getBootcamps.js
+++ controllers/bootcamps/getBootcamps.js
@@ -14,8 +14,9 @@ const getBootcamps = toHandleAsync(async (req, res, next) => {
   // create a copy of the query
   const requestQuery = { ...req.query };
 
-  // exclude select but will add again later
-  const removeFields = ['select'];
+  // exclude those not an operator gt|gte|lt|lte|in
+  // but will add again later
+  const removeFields = ['select', 'sort'];
   removeFields.forEach(param => delete requestQuery[param]);
 
   // convertto string: user query eg: bootcamps?averageCost[lte]=1000
@@ -33,6 +34,14 @@ const getBootcamps = toHandleAsync(async (req, res, next) => {
     const fields = req.query.select.split(',').join(' ');
     query = query.select(fields)
   }
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy)
+  }
+  // default sort
+  else { query = query.sort(`-createdAt`) }
+
 
   const foundBootcamps = await query;
 
