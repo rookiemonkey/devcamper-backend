69d27effbfb4ffb652e49769fe53f8264ca5fdd2
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-24 17:36:56 +0800

    added filter throug url queries

--- controllers/bootcamps/getBootcamps.js
+++ controllers/bootcamps/getBootcamps.js
@@ -7,7 +7,14 @@ const toHandleAsync = require('../../middlewares/toHandleAsync');
  */
 
 const getBootcamps = toHandleAsync(async (req, res, next) => {
-  const foundBootcamps = await Bootcamp.find({})
+
+  // user query eg: bootcamps?averageCost[lte]=1000
+  let queryString = JSON.stringify(req.query);
+
+  // transform into eg: {"averageCost":{"$lte":"1000"}}
+  queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  const foundBootcamps = await Bootcamp.find(JSON.parse(queryString))
 
   res
     .status(200)
