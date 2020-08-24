e81f61c19ecaa8da792d570bfe456fcee9afb609
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-24 13:45:36 +0800

    updateBootcamp works

--- controllers/bootcamps/updateBootcamp.js
+++ controllers/bootcamps/updateBootcamp.js
@@ -1,12 +1,27 @@
+const Bootcamp = require('../../models/Bootcamp');
+
 /**
  * @DESC update a bootcamp
  * @PATH PUT /api/v1/bootcamps/:bootcampId
  */
 
-const updateBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `updated bootcamp ${req.params.bootcampId}` });
+const updateBootcamp = async (req, res, next) => {
+  try {
+    const foundBootcamp = await Bootcamp.findByIdAndUpdate(
+      req.params.bootcampId, req.body, { new: true, runValidators: true }
+    )
+    if (!foundBootcamp) { throw new Error(`Bootcamp doesn't exists`) }
+
+    res
+      .status(200)
+      .json({ success: true, data: foundBootcamp });
+  }
+
+  catch (error) {
+    res
+      .status(400)
+      .json({ success: false, msg: `${error.message}` });
+  }
 };
 
 module.exports = updateBootcamp;
