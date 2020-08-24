43ef8341644644e2a39098b3ddd5f23dd190b67c
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-24 13:50:42 +0800

    deleteBootcamp works

--- controllers/bootcamps/deleteBootcamp.js
+++ controllers/bootcamps/deleteBootcamp.js
@@ -1,12 +1,25 @@
+const Bootcamp = require('../../models/Bootcamp');
+
 /**
  * @DESC delete a bootcamp
  * @PATH DELETE /api/v1/bootcamps/:bootcampId
  */
 
-const deleteBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `deleted a bootcamp ${req.params.bootcampId}` });
+const deleteBootcamp = async (req, res, next) => {
+  try {
+    const foundBootcamp = await Bootcamp.findByIdAndDelete(req.params.bootcampId)
+    if (!foundBootcamp) { throw new Error(`Bootcamp doesn't exists`) }
+
+    res
+      .status(200)
+      .json({ success: true, data: {} });
+  }
+
+  catch (error) {
+    res
+      .status(400)
+      .json({ success: false, msg: `${error.message}` });
+  }
 };
 
 module.exports = deleteBootcamp;
