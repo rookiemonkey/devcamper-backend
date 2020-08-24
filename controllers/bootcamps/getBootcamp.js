f18d1c5427faf9c727b390b03f71c2cbcb6a49f4
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-24 13:16:15 +0800

    getBootcamps works

--- controllers/bootcamps/getBootcamp.js
+++ controllers/bootcamps/getBootcamp.js
@@ -1,12 +1,24 @@
+const Bootcamp = require('../../models/Bootcamp');
+
 /**
  * @DESC get a bootcamp
  * @PATH GET /api/v1/bootcamps/:bootcampId
  */
 
-const getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `get a bootcamp ${req.params.bootcampId}` });
+const getBootcamp = async (req, res, next) => {
+  try {
+    const foundBootcamp = await Bootcamp.findById(req.params.bootcampId)
+
+    res
+      .status(200)
+      .json({ success: true, data: foundBootcamp });
+  }
+
+  catch (error) {
+    res
+      .status(400)
+      .json({ success: false, msg: `${error.message}` });
+  }
 };
 
 module.exports = getBootcamp;
