a4d7ee5d20b26ed87a47f0d642ada685bd7b5677
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-26 07:31:18 +0800

    added aggreagation for bootcamp averageRating

--- models/Review.js
+++ models/Review.js
@@ -13,8 +13,8 @@ const ReviewSchema = new mongoose.Schema({
     },
     rating: {
         type: Number,
-        min: 1,
-        max: 10,
+        min: [1, 'Rating must be atleast 1'],
+        max: [10, `Rating can't be more than 10`],
         required: [true, 'Review rating between 1 to 10']
     },
     bootcamp: {
@@ -31,5 +31,37 @@ const ReviewSchema = new mongoose.Schema({
 
 // user will only be able to add 1 review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
+
+// static method to getAverageRating 
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+    const result = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId } // review field to match
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageRating: { $avg: '$rating' }
+                // averageRating field from bootcamp
+                // aggregate the rating field from reviews
+            }
+        }
+    ])
+
+    // update the bootcamp's averageRating    
+    await this
+        .model('Bootcamp')
+        .findByIdAndUpdate(bootcampId, {
+            averageRating: Math.ceil(result[0].averageRating)
+        })
+}
+
+ReviewSchema.post('save', function () {
+    this.constructor.getAverageRating(this.bootcamp)
+})
+
+ReviewSchema.pre('remove', function () {
+    this.constructor.getAverageRating(this.bootcamp)
+})
 
 module.exports = mongoose.model('Review', ReviewSchema)
