429f8037df16ae9088b7774a97dbc1fa61458da3
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-25 18:26:18 +0800

    implemented password reset link sent on email

--- controllers/auth/forgotPassword.js
+++ controllers/auth/forgotPassword.js
@@ -1,6 +1,7 @@
 const User = require('../../models/User');
 const toHandleAsync = require('../../middlewares/toHandleAsync');
 const ErrorResponse = require('../../utils/class_error');
+const sendEmail = require('../../utils/sendEmail');
 
 /**
  * @DESC process a forgot password token to be emailed
@@ -15,9 +16,27 @@ const forgotPassword = toHandleAsync(async (req, res, next) => {
 
     await foundUser.save({ validateBeforeSave: false });
 
-    res
-        .status(200)
-        .json({ success: true, data: foundUser });
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetPassword/${resetToken}`;
+    const message = `A password reset has been requested. Please visit: ${resetUrl}`;
+
+    try {
+        await sendEmail({
+            email: foundUser.email,
+            subject: `Password Reset Token`,
+            message
+        })
+
+        return res
+            .status(200)
+            .json({ success: true, data: `Password Reset email sent!` });
+    }
+
+    catch (error) {
+        foundUser.resetPasswordToken = undefined;
+        foundUser.resetPasswordExpire = undefined;
+        await foundUser.save({ validateBeforeSave: false });
+        return next(new ErrorResponse(`Something went wrong upon sending the token to your email. Please request again`, 400));
+    }
 });
 
 module.exports = forgotPassword;
