5c6d5510caeb749039272fe1876e5d6856cdca93
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-26 08:35:28 +0800

    removed cors for the meantime

--- server.js
+++ server.js
@@ -40,12 +40,6 @@ app.options('*', cors(corsOptions))
 app.use(hpp());
 app.use(fileupload());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    next();
-});
-
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
