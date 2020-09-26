2b9651b0d62c73c622bc98449ea9e766e763c329
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-26 08:20:28 +0800

    removed cors for the meantime

--- server.js
+++ server.js
@@ -27,6 +27,13 @@ const corsOptions = {
     optionsSuccessStatus: 200
 }
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
