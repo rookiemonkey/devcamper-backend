31661e86a89ac1f2b866abf10d7b8ae674703cee
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-26 08:30:01 +0800

    removed cors for the meantime

--- server.js
+++ server.js
@@ -35,14 +35,13 @@ app.use(mongoSanitize());
 app.use(helmet());
 app.use(xssClean());
 app.use(rateLimiter);
-// app.use(cors());
-// app.options('*', cors())
+app.use(cors());
+app.options('*', cors())
 app.use(hpp());
 app.use(fileupload());
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
